5c68f3692a9d0a1c8e5657d7979028ea3d952bf7
darwishdev <a.darwish.dev@gmail.com>
2023-07-20 02:00:37 +0300

    use app router inside main

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 import DashKit from '@/DashKit'
 import '@/assets/scss/app.scss'
+import router from './router'
+
 
 const app = createApp(App)
 app.use(DashKit)
+    .use(router)
 app.mount('#app')
