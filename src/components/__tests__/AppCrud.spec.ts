5a8c2c0159ea4542c49b4ef24a0faa80833ee736
KareemWebDeveloper <kemoayman2015@gmail.com>
2023-07-30 11:08:23 +0300

    app crud tests

--- src/components/__tests__/AppCrud.spec.ts
+++ src/components/__tests__/AppCrud.spec.ts
@@ -2,6 +2,10 @@ import { CrudOptions } from './../../types/types';
 import { mount } from "@vue/test-utils";
 import apiClient from "@/api/ApiMock";
 import router from '@/router';
+<<<<<<< Updated upstream
+=======
+import { ref } from 'vue';
+>>>>>>> Stashed changes
 // import VueRouter from 'vue-router'
 import AppCrud from "@/components/AppCrud.vue";
 import { expect, describe, it, vi, afterEach, beforeEach } from "vitest";
@@ -9,11 +13,20 @@ import DashKit from '@/DashKit'
 import dashkitConfig from '@/dashkit.config'
 describe('App crud tests ', () => {
     let wrapper: any = null
+<<<<<<< Updated upstream
 
+=======
+    const errorHandler: any = {
+    
+    }
+    const filterModel = ref({ roleName: "asms" })
+    const modelDisplay = ref({})
+>>>>>>> Stashed changes
 
     let FormToastHandler: any = {
         hideToast: false
     }
+<<<<<<< Updated upstream
     let FormSubmitHandler: any = {
         submit: apiClient.userCreate,
 
@@ -90,6 +103,82 @@ describe('App crud tests ', () => {
         options : CrudOptions ,
         createForm : CreateForm,
         deleteRestoreHandler : DeleteRestoreHandler
+=======
+    const createFormSubmitHandler: any = {
+        submit: apiClient.userCreate,
+        errorHandler,
+    }
+    const crudOptions: any = {
+        title: 'roles_listt',
+        feature: 'roles',
+        showExportButton: true,
+        showCreateButton: true,
+        showDeletedFilter: true
+    }
+    const filterFormInputs:any = [
+        {
+            $formkit: 'text',
+            outerClass: "col-6",
+            name: 'roleName',
+            label: 'roleNameLabel',
+            placeholder: 'roleNamePlaceholder',
+    
+        },
+        {
+            $formkit: 'textarea',
+            outerClass: "col-6",
+            name: 'userName',
+            label: 'roleDescriptionLabel',
+            placeholder: 'roleDescriptionPlaceholder',
+        },
+    ]
+    const sections: any[] = [
+        {
+            role_info: [
+                {
+                    $formkit: 'text',
+                    outerClass: "col-4",
+                    name: 'username',
+                    label: 'roleNameLabel',
+                    placeholder: 'roleNamePlaceholder',
+                    validation: 'required|length:3',
+    
+                },
+                {
+                    $formkit: 'textarea',
+                    outerClass: "col-8",
+                    name: 'userPhone',
+                    label: 'roleDescriptionLabel',
+                    placeholder: 'roleDescriptionPlaceholder',
+                    validation: '',
+                },
+            ],
+        },
+    ]
+    const filterForm: any = {
+        options: {
+            showActiveFilters: true,
+            showClearFilters: true
+        },
+        inputs: filterFormInputs,
+        modelValue: filterModel,
+        displayValue: modelDisplay
+    }
+    const upateOptions: any = {
+        id: "role-update",
+        title: "role_update",
+    }
+    const createForm: any = {
+        sections: sections,
+        options: upateOptions,
+        submitHandler: createFormSubmitHandler,
+        toastHandler: FormToastHandler,
+    }
+    let propss = {
+        options : crudOptions ,
+        createForm : createForm,
+        filterForm : filterForm
+>>>>>>> Stashed changes
     }
     beforeEach(() => {
         router.push('/roles')
@@ -105,6 +194,7 @@ describe('App crud tests ', () => {
                 plugins: [[DashKit, dashkitConfig], router],
             },
             props: {
+<<<<<<< Updated upstream
                 options : CrudOptions ,
                 createForm : CreateForm,
                 deleteRestoreHandler : DeleteRestoreHandler
@@ -190,5 +280,73 @@ describe('App crud tests ', () => {
 //         expect(usernameInput.exists()).not.toBe(true)
 //         expect(userPhoneInput.exists()).not.toBe(true)
 //     })
+=======
+                options : crudOptions ,
+                createForm : createForm,
+                filterForm : filterForm
+            }
+        })
+        await new Promise(resolve => setTimeout(resolve, 1500))
+        expect(wrapper.text()).toContain('roles list')
+        expect(wrapper.text()).toContain('New')
+        expect(wrapper.text()).toContain('export')
+        const createButton = wrapper.find('button[aria-label="New"]') 
+        const exportButton = wrapper.find('button[aria-label="export"]') 
+        expect(createButton.exists()).toBe(true)
+        expect(exportButton.exists()).toBe(true)
+    })
+
+    it('a create form should pop up after clicking on create icon in case of passing the createForm prop', async () => {
+        wrapper = mount(AppCrud, {
+            global: {
+                plugins: [[DashKit, dashkitConfig], router],
+            },
+            props: {
+                options : crudOptions,
+                createForm : createForm,
+                filterForm : filterForm
+            }
+        })
+        await new Promise(resolve => setTimeout(resolve, 1500))
+        const createButton = wrapper.find('button[aria-label="New"]') 
+        expect(createButton.exists()).toBe(true)
+        await createButton.trigger('click')
+        await new Promise(resolve => setTimeout(resolve, 3000))
+        // const username = wrapper.find('input[name="username"]') 
+        // const userPhone = wrapper.find('input[name="userPhone"]') 
+        const CreateFormDialog = wrapper.findAll('.p-dialog');
+        CreateFormDialog.forEach(element => {
+        expect(element.exists()).toBe(true)
+        expect(element.contains('.formkit-form')).toBe(true)
+        expect(element.contains('input[name="userName"]')).toBe(true)
+        expect(element.contains('input[name="userPhone"]')).toBe(true)
+        })        
+    })
+
+    it('no update form should pop up after clicking on edit icon if we did not pass the updateForm prop', async () => {
+        router.push('/roles')
+        wrapper = mount(AppCrud, {
+            global: {
+                plugins: [[DashKit, dashkitConfig], router],
+            },
+            props: {
+                options : crudOptions,
+                filterForm : filterForm
+            }
+        })
+        router.push('/roles')
+        await new Promise(resolve => setTimeout(resolve, 1500))
+        const createButton = wrapper.find('button[aria-label="New"]') 
+        expect(createButton.exists()).toBe(true)
+        await createButton.trigger('click')
+        await new Promise(resolve => setTimeout(resolve, 3000))
+        const updateForm = wrapper.find('.formkit-form');
+        const usernameInput = wrapper.find('input[name="userName"]');
+        const userPhoneInput = wrapper.find('input[name="userPhone"]');
+        expect(updateForm.exists()).not.toBe(true)
+        expect(usernameInput.exists()).not.toBe(true)
+        expect(userPhoneInput.exists()).not.toBe(true)
+    })
+>>>>>>> Stashed changes
 
 })
